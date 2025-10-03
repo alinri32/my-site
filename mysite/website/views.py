0c2d0555e985b3dc92ec529d5277d91ad512b76a
alinri32 <e.alinri@gmail.com>
2025-10-03 16:09:27 +0330

    make render HTML Doc

--- mysite/website/views.py
+++ mysite/website/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
 
 def index_view(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request , 'index.html')
 
 def about_view(request):
-    return HttpResponse('my name is ali and im backend developer with lovly python Django')
+    return render(request, 'about.html')
 
 def contact_view(request):
-    return JsonResponse({'first name' : 'Ali' , 'lastname' : 'Noori'})
+    return render(request, 'contact.html')
