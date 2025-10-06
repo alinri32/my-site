b2d21fe4d0be9df401e735d42f55c82aa5398b7f
alinri32 <e.alinri@gmail.com>
2025-10-06 15:23:58 +0330

    make single blog posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from blog.models import post
 
 # Create your views here.
@@ -7,5 +7,12 @@ def blog_view (request):
     context = {'posts':posts}
     return render(request , '../templates/blog/blog-home.html',context)
 
-def blog_single(request):
-    return render(request , '../templates/blog/blog-single.html')
+def blog_single(request, pid):
+    posts = get_object_or_404(post , pk=pid)
+    context = {'posts' : posts}
+    return render(request , '../templates/blog/blog-single.html',context)
+
+# def test(request,pid):
+#     posts = post.objects.get(id=pid)
+#     context = {'posts' : posts}
+#     return render(request , '../templates/blog/test.html',context)
